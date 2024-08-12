c0720987e4987438dcf483bf551d22870cc311c7
Tal Gluck <talagluck@gmail.com>
2024-08-12 22:41:49 +0200

    added mouse buttons, modified thumb clusters

--- keyboards/fingerpunch/ximi/v1/keymaps/talagluck/keymap.c
+++ keyboards/fingerpunch/ximi/v1/keymaps/talagluck/keymap.c
@@ -29,20 +29,20 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * | Shft |   Z  |   X  |   C  |   V  |   B  |           |   N  |   M  |   ,  |   .  |   /  | Ctrl |
  * `-----------------------------------------'           `-----------------------------------------'
  *        ,------.         ,--------------------.    ,--------------------.          ,------.
- *        | MUTE |         | LOWER| Enter|  Del |    |BckSpc| Space| RAISE|          | MUTE |
+ *        | MUTE |         |Mouse1|BckSpc|Mouse2|    |Tab   | Space| Enter|          | MUTE |
  *        `------'         `--------------------'    `--------------------.          `------'
  *                         ,--------------------.    ,--------------------.
- *                         |Mouse3|Mouse1|Mouse2|    | Vol- | Mute | Vol+ |    // 3 way thumb switch
+ *                         |NO_OP | NO_OP |NO_OP|    | Vol- | Mute | Vol+ |    // 3 way thumb switch
  *                         `--------------------'    `--------------------.
  */
 
 // Default config uses home row mods. So hold each of the keys on the home row to use ctrl, gui, alt, or shift
 [_QWERTY] = LAYOUT_ximi(
   KC_ESC,       KC_Q,         KC_W,         KC_E,         KC_R,         KC_T,         KC_Y,    KC_U,         KC_I,         KC_O,         KC_P,            KC_BSLS,
-  KC_TAB,       LCTL_T(KC_A), LALT_T(KC_S), LGUI_T(KC_D), LSFT_T(KC_F), KC_G,         KC_H,    RSFT_T(KC_J), RGUI_T(KC_K), RALT_T(KC_L), RCTL_T(KC_SCLN), KC_QUOT,
+  KC_TAB,       LCTL_T(KC_A), LALT_T(KC_S), LGUI_T(KC_D), LSFT_T(KC_F), HYPR_T(KC_G), HYPR_T(KC_H),    RSFT_T(KC_J), RGUI_T(KC_K), RALT_T(KC_L), RCTL_T(KC_SCLN), KC_QUOT,
   KC_LSFT,      KC_Z,         KC_X,         KC_C,         KC_V,         KC_B,         KC_N,    KC_M,         KC_COMM,      KC_DOT,       KC_SLSH,         KC_RCTL,
-                KC_MUTE,                    LOWER,        KC_ENT,       KC_DEL,       KC_BSPC, KC_SPC,       RAISE,                      KC_MUTE,
-                                            KC_MS_BTN3,   KC_MS_BTN1,   KC_MS_BTN2,   KC_VOLD, KC_MUTE,      KC_VOLU
+                KC_MUTE,                    LT(LOWER, KC_MS_BTN1),        KC_BSPC,       KC_MS_BTN2,       KC_TAB, KC_SPC,       LT(RAISE, KC_ENT),                      KC_MUTE,
+                                            KC_NO,        KC_NO,        KC_NO,        KC_VOLD, KC_MUTE,      KC_VOLU
 ),
 
 /* Colemak
@@ -58,7 +58,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *        | MUTE |         | LOWER| Enter|   '  |    |BckSpc| Space| RAISE|          | MUTE |
  *        `------'         `--------------------'    `--------------------.          `------'
  *                         ,--------------------.    ,--------------------.
- *                         | Undo | Redo2| Redo |    | Vol- | Mute | Vol+ |    // 3 way thumb switch
+ *                         |NO_OP | NO_OP |NO_OP|    | Vol- | Mute | Vol+ |    // 3 way thumb switch
  *                         `--------------------'    `--------------------.
  */
 
@@ -81,7 +81,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * | ____ |  Ctrl|   `  |  GUI |  Alt |      |           |      |      |      |   \  |   '  | ____ |
  * `-----------------------------------------'           `-----------------------------------------'
  *        ,------.         ,--------------------.    ,--------------------.          ,------.
- *        | MUTE |         | LOWER|      |      |    |      |      | RAISE|          | MUTE |
+ *        | MUTE |         | LOWER|KC_DEL|      |    |      |      | RAISE|          | MUTE |
  *        `------'         `--------------------'    `--------------------.          `------'
  *                         ,--------------------.    ,--------------------.
  *                         | Undo | Redo2| Redo |    | Vol- | Mute | Vol+ |    // 3 way thumb switch
@@ -91,7 +91,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   _______,      KC_1,    KC_2,    KC_3,    KC_4,    KC_5,         KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    _______,
   _______,      KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_TAB,       _______, KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, _______,
   _______,      KC_LCTL, KC_GRV,  KC_LGUI, KC_LALT, _______,      _______, _______, _______, KC_BSLS, KC_QUOT, _______,
-                _______,          _______, _______, _______,      _______, _______, _______,          _______,
+                _______,          _______, KC_DEL,  _______,      _______, _______, _______,          _______,
                                   _______, _______, _______,      _______, _______, _______
 ),
 
