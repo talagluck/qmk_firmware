4555d2b2e6396c843ed581d7d33a9da17d7db59a
Tal Gluck <talagluck@gmail.com>
2024-10-14 20:09:22 +0200

    previous iteration

--- keyboards/fingerpunch/ximi/v1/keymaps/talagluck/keymap.c
+++ keyboards/fingerpunch/ximi/v1/keymaps/talagluck/keymap.c
@@ -41,7 +41,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_ESC,       KC_Q,         KC_W,         KC_E,         KC_R,         KC_T,         KC_Y,    KC_U,         KC_I,         KC_O,         KC_P,            KC_BSLS,
   KC_TAB,       LCTL_T(KC_A), LALT_T(KC_S), LGUI_T(KC_D), LSFT_T(KC_F), HYPR_T(KC_G), HYPR_T(KC_H),    RSFT_T(KC_J), RGUI_T(KC_K), RALT_T(KC_L), RCTL_T(KC_SCLN), KC_QUOT,
   KC_LSFT,      KC_Z,         KC_X,         KC_C,         KC_V,         KC_B,         KC_N,    KC_M,         KC_COMM,      KC_DOT,       KC_SLSH,         KC_RCTL,
-                KC_MUTE,                    LT(LOWER, KC_MS_BTN1),        KC_BSPC,       KC_MS_BTN2,       KC_TAB, KC_SPC,       LT(RAISE, KC_ENT),                      KC_MUTE,
+                KC_MUTE,                    LT(_LOWER, KC_MS_BTN1),        KC_BSPC,       KC_MS_BTN2,       KC_TAB, KC_SPC,       LT(_RAISE, KC_ENT),                      KC_MUTE,
                                             KC_NO,        KC_NO,        KC_NO,        KC_VOLD, KC_MUTE,      KC_VOLU
 ),
 
@@ -91,7 +91,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   _______,      KC_1,    KC_2,    KC_3,    KC_4,    KC_5,         KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    _______,
   _______,      KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_TAB,       _______, KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, _______,
   _______,      KC_LCTL, KC_GRV,  KC_LGUI, KC_LALT, _______,      _______, _______, _______, KC_BSLS, KC_QUOT, _______,
-                _______,          _______, KC_DEL,  _______,      _______, _______, _______,          _______,
+                _______,          LT(_ADJUST, KC_MS_BTN1), KC_DEL,  _______,      _______, _______, _______,          _______,
                                   _______, _______, _______,      _______, _______, _______
 ),
 
@@ -115,7 +115,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   _______,      KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,      KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, _______,
   _______,      KC_ESC,  _______, _______, _______, EE_CLR,       _______, KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR, _______,
   _______,      KC_CAPS, KC_TILD, _______, QK_RBT,  QK_BOOT,      _______, _______, _______, KC_PIPE,  KC_DQT, _______,
-                _______,          _______, _______, _______,      KC_DEL,  _______, _______,          _______,
+                _______,          _______, _______, _______,      KC_DEL,  _______, LT(_ADJUST, KC_ENT),          _______,
                                   _______, _______, _______,      _______, _______, _______
 ),
 
